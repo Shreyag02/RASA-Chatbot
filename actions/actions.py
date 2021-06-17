a444315265e3f92a0cbd8fc3c443a01f9fedee4c
prashantmathurr <prashantmathur1999@gmail.com>
2021-06-18 02:52:30 +0530

    stable branch

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,26 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_clubs"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        club = tracker.get_slot('club')
+        url = "https://abes.ac.in"
+
+        if club == "Technical":
+            url = "https://abes.ac.in"
+
+        dispatcher.utter_message({"text": url, "image": "https://i.imgur.com/nGF1K8f.jpg"})
+        return []
